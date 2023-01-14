44e8570d79715b725a32f5b8e7f974ff5243144d
Mizut452 <ssvror444@gmail.com>
2023-01-15 06:39:05 +0900

    test ランダムで出た数字を出力（数字が被った場合は消す）

--- src/main/java/Mizut452/quiz_app/test.java
+++ src/main/java/Mizut452/quiz_app/test.java
@@ -1,9 +1,6 @@
 package Mizut452.quiz_app;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class test {
     List<Integer> putQuestionId = new ArrayList<>();
@@ -11,33 +8,37 @@ public class test {
     public static void main(String[] args) {
         Random random = new Random();
         List<Integer> listQuestionId = new ArrayList<>();
+        int num = 0;
         //ここで示した数の数字がランダムに出力される
         int listLength = 10;
-        for(int i = 0; i < 10;) {
+        for (int i = 0; i < 10; ) {
             //1~10が乱数
-            int randomInt = random.nextInt(listLength) +1;
+            int randomInt = random.nextInt(listLength) + 1;
             if (i == 0) {
                 listQuestionId.add(randomInt);
                 i++;
-            }
-
-            if(i > 0) {
-                int prev = listQuestionId.get(i -1);
-
-                if (prev == randomInt) {
-                    System.out.println("数字が被りました");
-                } else {
+            } else {
+                int count =0;
+                for(int c : listQuestionId) {
+                    if (c == randomInt) {
+                        count++;
+                    }
+                }
+                if (count == 0) {
                     listQuestionId.add(randomInt);
                     i++;
+                } else {
+                    System.out.println("www");
                 }
             }
         }
+        System.out.println(listQuestionId.size());
         System.out.println(listQuestionId.get(0));
         System.out.println(listQuestionId.get(1));
         System.out.println(listQuestionId.get(2));
         System.out.println(listQuestionId.get(3));
         System.out.println(listQuestionId.get(4));
         System.out.println(listQuestionId.get(5));
-
     }
 }
+
