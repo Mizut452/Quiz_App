4d07a953b8b26f1c8133872efbfe6e1972288ca2
Mizut452 <ssvror444@gmail.com>
2023-01-23 01:15:04 +0900

    Login機構途中

--- src/main/java/Mizut452/quiz_app/Controller/LoginUserController.java
+++ src/main/java/Mizut452/quiz_app/Controller/LoginUserController.java
@@ -1,6 +1,7 @@
 package Mizut452.quiz_app.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,5 +9,17 @@ public class LoginUserController {
     @RequestMapping("/createaccount")
     public String createAccount() {
         return "loginUser/createAccount";
+    }
+
+    @PostMapping("/create")
+    public String createMethod() {
+
+        return "redirect:/create/roles";
+    }
+
+    @PostMapping("/create/roles")
+    public String createRoles() {
+
+        return "createComplete";
     }
 }
